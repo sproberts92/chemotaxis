fe244039b62643dd98e7949fa3c796bfb1f81755
Samuel Roberts <sproberts92@outlook.com>
2016-03-15 11:23:09 +0100

    Add joblib for parallelisation and tqdm for progress bar

--- vis.py
+++ vis.py
@@ -1,9 +1,16 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from tqdm import tqdm
+from tqdm import trange
+from joblib import Parallel, delayed
 
-for i in tqdm(range(500)):
+def main():
+	Parallel(n_jobs=4)(delayed(img_process)(i) for i in trange(5000))
+		
+def img_process(i):
 	arr = np.loadtxt("output/pcount_{0}.dat".format(i))
 	plt.matshow(arr)
 	plt.savefig("frames/pcount_{0}.png".format(i))
 	plt.close()
+
+if __name__ == '__main__':
+	main()
