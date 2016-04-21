1351310e685a37acaa9f0be0343f7c1d741684e0
Samuel Roberts <sproberts92@outlook.com>
2016-04-21 17:47:18 +0200

    Add command line argument for selecting file to plot

--- visualisation/vis_hist.py
+++ visualisation/vis_hist.py
@@ -1,12 +1,16 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import sys
 
-def main():
-	arr = np.loadtxt("output/visited_hist.dat")
-	plt.plot(arr)
-	plt.show()
-	# plt.savefig("frames/total_visits.png")
-	plt.close()
+def main(argv):
+	if len(argv) > 1:
+		arr = np.loadtxt(argv[1])
+		plt.plot(arr)
+		plt.show()
+		# plt.savefig("frames/total_visits.png")
+		plt.close()
+	else:
+		print("Specify input file.")
 
 if __name__ == '__main__':
-	main()
+	main(sys.argv)
