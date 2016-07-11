5b28bc26b7b681d508b24b31d42ac2e31538ec6f
Samuel Roberts <sproberts92@outlook.com>
2016-07-11 15:23:31 +0200

    Finalise formatting of plots
    Remove borders and fix aspect ratios for use in thesis.

--- visualisation/vis.py
+++ visualisation/vis.py
@@ -4,12 +4,20 @@ from tqdm import trange
 from joblib import Parallel, delayed
 
 def main():
-	Parallel(n_jobs=4)(delayed(img_process)(i) for i in trange(5000))
-		
+	Parallel(n_jobs=4)(delayed(img_process)(i) for i in trange(1000))
+
 def img_process(i):
 	arr = np.loadtxt("output/pcount_{0}.dat".format(i))
-	plt.matshow(arr)
-	plt.savefig("frames/pcount_{0}.png".format(i))
+
+	plt.figure(frameon=False)
+
+	ax = plt.axes()
+	plt.imshow(arr, interpolation='nearest')
+
+	ax.set_xticks([]); ax.set_yticks([]);
+	plt.axis('off')
+
+	plt.savefig("frames/check_rand/pcount_{0}.png".format(i), bbox_inches='tight', pad_inches=0)
 	plt.close()
 
 if __name__ == '__main__':
--- visualisation/vis_total.py
+++ visualisation/vis_total.py
@@ -3,8 +3,17 @@ import matplotlib.pyplot as plt
 
 def main():
 	arr = np.loadtxt("output/total_visits.dat")
-	plt.matshow(arr)
-	plt.savefig("frames/total_visits.png")
+
+	plt.figure(frameon=False)
+	ax = plt.axes()
+
+	plt.imshow(arr)
+
+	ax.set_xticks([]); ax.set_yticks([]);
+	plt.axis('off')
+
+	plt.savefig("frames/total_visits.png", bbox_inches='tight', pad_inches=0)
+	# plt.show()
 	plt.close()
 
 if __name__ == '__main__':
